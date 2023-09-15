bdd6e3926ac0545b1e101482cbd22c6cac5df0bc
Nithin <ankamnithin1998@gmail.com>
2023-09-16 02:03:54 +0530

    added new files(updated all files)

---
+++ App.js
@@ -0,0 +1,77 @@
+// ## Namaste React Course by Akshay Saini
+// # Chapter 07 - Finding the Path
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./src/Components/Header";
+import Body from "./src/Components/Body";
+import Footer from "./src/Components/Footer";
+import About from "./src/Components/About";
+import Error from "./src/Components/Error";
+import Contact from "./src/Components/Contact";
+import Login from "./src/Components/Login";
+import RestaurantMenu from "./src/Components/RestaurantMenu";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"; // for routing our page import createBrowserRouter and RouterProvider for providing router & Outlet for children component for nested routing
+
+/* My Food App structure will look like this, 
+            1) Header
+                - Logo
+                - Nav Items(right side)
+                - Cart
+            2) Body
+                - Search bar
+                - Restaurants List
+                    - Restaurant card
+                        - Image
+                        - Name
+                        - Rating
+            3) Footer
+                - Links
+                - Copyrights
+       
+*/
+
+// AppLayout component to render: Header, Outlet(it contain children component like body, About, Restaurant Menu etc) and Footer Component
+export const AppLayout = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Outlet />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+// call createBrowserRouter for routing different pages
+const appRouter = createBrowserRouter([
+  {
+    path: "/", // show path for routing
+    element: <AppLayout />, // show component for particular path
+    errorElement: <Error />, // show error component for path is different
+    children: [
+      // show children component for routing
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(<RouterProvider router={appRouter} />); // render RouterProvider and use router as props and pass value appRouter
---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+// Footer component for footer section
+const Footer = () => {
+  return (
+    <div className="footer">
+      Created By
+      <i className="fa-solid fa-heart"></i>
+      <a href="https://www.linkedin.com/in/nithinkumar-ank/" target="_blank" title="Nithin Kumar's Linkedin Profile" rel="noreferrer">
+        Nithin Kumar
+      </a>
+      <i className="fa-solid fa-copyright"></i>2023
+      <strong>
+        Food<span>Fire</span>
+      </strong>
+    </div>
+  );
+};
+
+export default Footer;
